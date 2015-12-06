35617af2854012355f86adbf0e7c87d8c7ee4489
Guðni <gudni.gardarsson@gmail.com>
2015-12-06 21:30:50 +0000

    refactor: made handler to manage command names in command handler

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,47 +1,51 @@
 module.exports = function tictactoeCommandHandler(events) {
   var gameCreatedEvent = events[0];
 
+  var handler = {
+    "CreateGame": function(command) {
+      return [{
+        cid: command.cid,
+        event: "GameCreated",
+        user: command.user,
+        name: command.name,
+        time: command.time
+      }]
+    },
+    "JoinGame": function(command) {
+      if(gameCreatedEvent === undefined) {
+        return [{
+          cid: command.cid,
+          event: "NoSuchGame",
+          user: command.user,
+          name: command.name,
+          time: command.time
+        }]
+      }
+      return [{
+        cid: command.cid,
+        event: "GameJoined",
+        user: command.user,
+        name: command.name,
+        hostingUser: gameCreatedEvent.user,
+        time: command.time
+      }]
+    },
+    "Place": function(command) {
+      return [{
+        cid: command.cid,
+        event: "Placed",
+        user: command.user,
+        symbol: command.symbol,
+        row: command.row,
+        column: command.column,
+        time: command.time
+      }]
+    }
+  };
+
   return {
     executeCommand: function(command) {
-      if(command.command === "CreateGame") {
-        return [{
-          cid: command.cid,
-          event: "GameCreated",
-          user: command.user,
-          name: command.name,
-          time: command.time
-        }]
-      }
-      else if(command.command === "JoinGame") {
-        if(gameCreatedEvent === undefined) {
-          return [{
-            cid: command.cid,
-            event: "NoSuchGame",
-            user: command.user,
-            name: command.name,
-            time: command.time
-          }]
-        }
-        return [{
-          cid: command.cid,
-          event: "GameJoined",
-          user: command.user,
-          name: command.name,
-          hostingUser: gameCreatedEvent.user,
-          time: command.time
-        }]
-      }
-      else if(command.command === "Place") {
-        return [{
-          cid: command.cid,
-          event: "Placed",
-          user: command.user,
-          symbol: command.symbol,
-          row: command.row,
-          column: command.column,
-          time: command.time
-        }]
-      }
+      return handler[command.command](command);
     }
   };
 };
