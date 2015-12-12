a3547e1a1359c7248edb643b27fa40a71771eca0
Guðni <gudni.gardarsson@gmail.com>
2015-12-12 13:06:41 +0000

    pushing fluid api test to see it fail

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -52,10 +52,10 @@ describe('TEST ENV GET /api/gameHistory', function () {
 
 
    it('Should execute fluid API test', function (done) {
-     /*
+
      given(user("YourUser").createsGame("TheFirstGame"))
      .expect("GameCreated").withName("TheFirstGame").isOk(done);
-      */
+
      done();
    });
 
