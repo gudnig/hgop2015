5d4bc5a34f1e2740c7139f8b0e234b52c7ff2a03
Guðni <gudni.gardarsson@gmail.com>
2015-12-17 18:18:08 +0000

    getting acceptance stage to run on jenkins

--- loadtest.sh
+++ loadtest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
-
-export ACCEPTANCE_URL=http://192.168.33.11:8080
+echo restarting docker image
+ssh vagrant@$1 "docker restart yoserver"
+echo running capacity test
+export ACCEPTANCE_URL=http://$1:$2
 grunt mochaTest:load
