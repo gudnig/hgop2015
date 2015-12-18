06db8f201186fe7cd86507f7f27e3415859d9777
Guðni <gudni.gardarsson@gmail.com>
2015-12-18 01:54:27 +0000

    Fixing acceptance stage script to work even with no prior docker process

--- resetdocker.sh
+++ resetdocker.sh
@@ -3,7 +3,12 @@
 #usage: ./resetdocker.sh version port container
 set -e - pipefail
 
-docker kill tictactoe
-docker rm tictactoe
+old=`docker ps -q`
+if [ $old ]; then
+  docker kill $old
+  docker rm $old
+fi
+
+
 docker pull $3:$1
 docker run -p $2:8080 -d -e "NODE_ENV=production" --name tictactoe $3:$1
