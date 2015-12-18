cb8ca67938c0310a000d85fb07401f1e3817787a
Guðni <gudni.gardarsson@gmail.com>
2015-12-18 03:04:56 +0000

    fixed capacity test to match all the script changes and new container name

--- loadtest.sh
+++ loadtest.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# runs capaicty tests on host after restarting docker container
+#usage ./loadtest.sh host container
 set -e -o pipefail
 HOST="192.168.33.11"
+CONTAINER="tictactoe"
 if [ -n "$1" ]; then
   HOST="$1"
 fi
+
+HOST="192.168.33.11"
+if [ -n "$2" ]; then
+  CONTAINER="$2"
+fi
 echo restarting docker image
-ssh vagrant@$HOST "docker restart yoserver"
+ssh vagrant@$HOST "docker restart $CONTAINER"
 echo running capacity test
 grunt mochaTest:load
